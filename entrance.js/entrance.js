297b477d7f331c5861f8d7a1b25e877321f0a155
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-03-01 21:22:31 +0000

    in a good starting position

--- entrance.js/entrance.js
+++ entrance.js/entrance.js
@@ -1,4 +1,7 @@
 // entrance.js - copyright James Hunt 2018 - 2018 / Some rights reserved
 $(document).ready(function() {
-    
+    // Check is a specific element is currently in the user's viewport
+    function isVisible(elem) {
+
+    }
 });
