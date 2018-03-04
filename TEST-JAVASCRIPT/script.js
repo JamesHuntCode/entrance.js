e160e23e3504a1e7a88cacc8c4d00c5fd03b5ea1
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-03-04 21:20:58 +0000

    functional viewport checker

---
+++ TEST-JAVASCRIPT/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        // Debugging happens in here...
+    });
+});
--- entrance.js/entrance.js
+++ entrance.js/entrance.js
@@ -1,7 +1,18 @@
 // entrance.js - copyright James Hunt 2018 - 2018 / Some rights reserved
-$(document).ready(function() {
-    // Check is a specific element is currently in the user's viewport
-    function isVisible(elem) {
 
-    }
-});
+// Return boolean value indicating if an element is on within the user's viewport
+function isVisible(element) {
+    var viewport = $(window);
+
+    var topOfViewport = $(window).scrollTop();
+    var bottomOfViewport = topOfViewport + viewport.height();
+
+    var topOfElement = element.offset().top;
+    var bottomOfElement = topOfElement + element.height();
+
+    var ease = element.height() / 2;
+
+    var isInsideViewport = ((topOfElement + ease >= topOfViewport) && (bottomOfElement - ease <= bottomOfViewport));
+
+    return isInsideViewport;
+}
